2019a24d437626f28c3faf1b4099ae310a004a01
beingsuperior <beingsuperior99@gmial.com>
2022-09-13 00:18:17 +0500

    last commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server sucessfully running on port: ${PORT}`)
+    )
+  )
+  .catch((e) => console.log(e.message));
+
+mongoose.set("useFindAndModify", false);
